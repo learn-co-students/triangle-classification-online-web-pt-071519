74c1383ba088567595d9d6518153290b0616a5e2
Colleen Halpenny <cp.halpenny@gmail.com>
2019-08-19 01:16:05 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,19 @@
 class Triangle
-  # write code here
+
+  def intialize(a, b, c)
+
+  end
+
+  def kind
+    :equilateral
+    :isosceles
+    :scalene
+  end
+
+  class TriangleError < StandardError
+    def message
+      "This is not a valid triangle"
+    end
+  end
+
 end
