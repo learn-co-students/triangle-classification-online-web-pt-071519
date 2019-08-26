0d222013013207491edb67ceccc49a20e8db8cf2
Renee Sims <info@reneesoul.com>
2019-08-26 17:16:52 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,20 @@
 class Triangle
-  # write code here
+  
+  attr_accessor :eq_length, :is_length, :sc_length
+  
+  def initialize(eq_length, is_length, sc_length)
+    
+  end 
+  
+  def kind 
+    :equilateral
+    :isosceles
+    :scalene
+    
+  end 
+  
+  class TriangleError < StandardError
+    def message
+      "Not a triangle, sorry."
+    end
 end
