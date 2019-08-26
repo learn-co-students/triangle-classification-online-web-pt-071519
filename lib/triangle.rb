01c8a4055ccc8c48a32a0ac96786ecb4cd58bd2e
Renee Sims <info@reneesoul.com>
2019-08-26 18:58:58 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,20 +1,37 @@
+require 'pry'
+
 class Triangle
   
-  attr_accessor :eq_length, :is_length, :sc_length
+  attr_accessor :triangle, :length, :eq_length, :eq_triangle, :is_triangle, :sc_triangle
   
-  def initialize(eq_length, is_length, sc_length)
-    
+  def initialize (side_one, side_two, side_three)
+  # binding.pry 
+    @triangle = side_one + side_two + side_three
+ 
   end 
+ 
+  def kind(triangle)
+binding.pry 
+    self.triangle = triangle
+     
+   
+
   
-  def kind 
-    :equilateral
-    :isosceles
-    :scalene
-    
+    if triangle.class != Triangle
+      begin
+        raise TriangleError
+      rescue TriangleError => error 
+        puts error.message
+      end
+    else
+      triangle.kind = self
+    end
   end 
   
   class TriangleError < StandardError
     def message
       "Not a triangle, sorry."
     end
+  end
+  
 end
