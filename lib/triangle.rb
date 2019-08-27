81d47ba128b3ab291a846bcc55de86078450d33e
Renee Sims <info@reneesoul.com>
2019-08-27 18:18:45 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,14 +11,14 @@ class Triangle
 
   end
   
-  def validates
+  def validation                  # DONT BE AFRAID TO SEPARATE THE RESULTS YOU WANT IN TO A MODULE
     real_triangle = [(side_one + side_two > side_three), (side_one + side_three > side_two), (side_two + side_three >  side_one)]
     [ side_one, side_two, side_three].each do |side|
       real_triangle << false if side <= 0 
       raise TriangleError if real_triangle.include?(false)
     end
     
-    @triangle_sides = []
+    @triangle_sides = []          # great use of uniq as a point of triangle definition
     @triangle_sides << side_one
     @triangle_sides << side_two
     @triangle_sides << side_three   
@@ -26,14 +26,11 @@ class Triangle
 
 
   def kind
-    validates
-    if (@triangle_sides.uniq).length == 1
-      return :equilateral  
-    elsif @triangle_sides.uniq.count == 2 
-      return :isosceles 
-    elsif @triangle_sides.uniq.length == 3 
-      return :scalene   
-    end
+    validation
+    return :equilateral if (@triangle_sides.uniq).length == 1
+    return :isosceles if @triangle_sides.uniq.count == 2 
+    return :scalene if @triangle_sides.uniq.length == 3 
+        
   end
       
       
