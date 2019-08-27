174ae08d5bee879890d4770367763a0461e2c12d
Renee Sims <info@reneesoul.com>
2019-08-27 18:13:52 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -2,36 +2,44 @@ require 'pry'
 
 class Triangle
   
-  attr_accessor :triangle, :length, :eq_length, :eq_triangle, :is_triangle, :sc_triangle
+  attr_accessor :side_one, :side_two, :side_three, :triangle_sides
   
-  def initialize (side_one, side_two, side_three)
-  # binding.pry 
-    @triangle = side_one + side_two + side_three
- 
-  end 
- 
-  def kind(triangle)
-binding.pry 
-    self.triangle = triangle
-     
-   
+  def initialize(side_one, side_two, side_three)
+    @side_one = side_one
+    @side_two = side_two
+    @side_three = side_three
 
-  
-    if triangle.class != Triangle
-      begin
-        raise TriangleError
-      rescue TriangleError => error 
-        puts error.message
-      end
-    else
-      triangle.kind = self
-    end
-  end 
-  
-  class TriangleError < StandardError
-    def message
-      "Not a triangle, sorry."
-    end
   end
   
+  def validates
+    real_triangle = [(side_one + side_two > side_three), (side_one + side_three > side_two), (side_two + side_three >  side_one)]
+    [ side_one, side_two, side_three].each do |side|
+      real_triangle << false if side <= 0 
+      raise TriangleError if real_triangle.include?(false)
+    end
+    
+    @triangle_sides = []
+    @triangle_sides << side_one
+    @triangle_sides << side_two
+    @triangle_sides << side_three   
+  end
+
+
+  def kind
+    validates
+    if (@triangle_sides.uniq).length == 1
+      return :equilateral  
+    elsif @triangle_sides.uniq.count == 2 
+      return :isosceles 
+    elsif @triangle_sides.uniq.length == 3 
+      return :scalene   
+    end
+  end
+      
+      
+  class TriangleError < StandardError
+    # def message
+    #   "Not a triangle, sorry."
+    # end
+  end
 end
