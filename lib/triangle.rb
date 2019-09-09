c397762b98c126595df2c730cedde8818de8f67c
Winston  Chung <18.chung.winston@ctkprep.org>
2019-09-09 03:35:12 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,27 +1,29 @@
 class Triangle
   
-  attr_accessor :s1, :s2, :s3
+  attr_accessor :a, :b, :c
   
-  def initialize(s1, s2, s3)
-    @s1 = s1
-    @s2 = s2
-    @s3 = s3
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
   end
   
-  def kind 
-  if((s1 == 0) && (s2 == 0) && (s3 == 0)) || ((s1 == 3) && (s2 == 4) && (s3 == -5)) || ((s1 == 1) && (s2 == 1) && (s3 == 3)) || ((s1 == 2) && (s2 == 4) && (s3 == 2)) || ((s1 == 7) && (s2 == 3) && (s3 == 2))
-    raise TriangleError
-  else
-    if ((s1 == s2) && (s1 == s3) && (s2 == s3))
-      return :equilateral
-    elsif ((s1 == s2) || (s1 == s3) || (s2 == s3))
-      return :isosceles
-    elsif ((s1 != s2) && (s1 != s3) && (s2 != s3))
-      return :scalene
+  def kind
+    validate_triangle
+    if a == b && b == c
+      :equilateral
+    elsif a == b || b == c || a == c
+      :isosceles
     else
-      raise TriangleError
+      :scalene
     end
   end
+  def validate_triangle
+    real_triangle = [(a + b > c), (a + c > b), (b + c > a)]
+    [a, b, c].each do |side|
+      real_triangle << false if side <= 0 
+    raise TriangleError if real_triangle.include?(false)
+    end
   end
   
   class TriangleError < StandardError
